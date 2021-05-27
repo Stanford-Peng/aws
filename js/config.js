d42bc042509adf1ca53aab2ea5ec5a1008b164b4
Kang Hsing <akhsi1@student.monash.edu>
2021-05-27 18:40:13 +1000

    Added Modify Tags feature

--- js/config.js
+++ js/config.js
@@ -7,8 +7,8 @@ window._config ={
     api:{
         uploadUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/upload',         //HTTP POST     JSON Format: {name:"xxxx.jpg", file:""}
         findByTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findbytags', //HTTP POST     JSON Format: {tags: "["person", "elephant"]"}
-        deleteUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/deleteimages',          //HTTP DELETE   JSON FORMAT: {name: ["xxxxx.jpg", "asdf.jpg"]}
+        deleteUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/deleteimages',       //HTTP DELETE   JSON FORMAT: {name: ["xxxxx.jpg", "asdf.jpg"]}
         findTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findtags',     //HTTP GET
-        modifyTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/modify-tags',//HTTP POST     JSON FORMAT: {?}
+        modifyTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/modify-tags',//HTTP POST     JSON FORMAT: {name: "xxx.jpg", tags: ["tag1", "tag2"]}
     }
 };
