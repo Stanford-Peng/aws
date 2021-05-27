0b60e44df8e61f07cdc0186378d4f14769e47130
Kang Hsing <akhsi1@student.monash.edu>
2021-05-27 15:44:15 +1000

    Fixed CORS problems on Delete

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ window._config ={
     api:{
         uploadUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/upload',         //HTTP POST     JSON Format: {name:"xxxx.jpg", file:""}
         findByTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findbytags', //HTTP POST     JSON Format: {tags: "["person", "elephant"]"}
-        deleteUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/query',          //HTTP DELETE   JSON FORMAT: {name: "xxxxx.jpg"}
+        deleteUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/deleteimages',          //HTTP DELETE   JSON FORMAT: {name: ["xxxxx.jpg", "asdf.jpg"]}
         findTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findtags',     //HTTP GET
         modifyTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/modify-tags',//HTTP POST     JSON FORMAT: {?}
     }
