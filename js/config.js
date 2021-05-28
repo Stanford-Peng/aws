ea4432d3704d950aa8ceb42d2d7344d2439445d4
Kang Hsing <akhsi1@student.monash.edu>
2021-05-29 03:16:56 +1000

    Added search by image but there's a bug (Internal server error)

--- js/config.js
+++ js/config.js
@@ -5,10 +5,11 @@ window._config ={
         clientId: '4rrtmgavid4rq03c4s80uist78'
     },
     api:{
-        uploadUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/upload',         //HTTP POST     JSON Format: {name:"xxxx.jpg", file:""}
-        findByTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findbytags', //HTTP POST     JSON Format: {tags: "["person", "elephant"]"}
-        deleteUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/deleteimages',       //HTTP DELETE   JSON FORMAT: {name: ["xxxxx.jpg", "asdf.jpg"]}
-        findTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findtags',     //HTTP GET
-        modifyTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/modify-tags',//HTTP POST     JSON FORMAT: {name: "xxx.jpg", tags: ["tag1", "tag2"]}
+        uploadUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/upload',                 //HTTP POST     JSON Format: {name:"xxxx.jpg", file:""}
+        findByTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findbytags',         //HTTP POST     JSON Format: {tags: "["person", "elephant"]"}
+        deleteUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/deleteimages',               //HTTP DELETE   JSON FORMAT: {name: ["xxxxx.jpg", "asdf.jpg"]}
+        findTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findtags',             //HTTP GET
+        modifyTagsUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/modify-tags',        //HTTP POST     JSON FORMAT: {name: "xxx.jpg", tags: ["tag1", "tag2"]}
+        findTagsByImageUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/findtagsbyimage'//HTTP POST     JSON Format: {file:""}
     }
 };
