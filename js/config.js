857a01d7852edc6de013d22ac7dce0fff39861e4
Kang Hsing <akhsi1@student.monash.edu>
2021-05-30 18:03:53 +1000

    Added federated authentication

--- js/config.js
+++ js/config.js
@@ -1,8 +1,11 @@
 window._config ={
     cognito:{
         userPoolId: 'us-east-1_wR9mama0w',
+        identityPoolId: 'us-east-1:caa1ec19-b4ce-4188-a6d3-dfd244ec662f',
         region: 'us-east-1',
-        clientId: '4rrtmgavid4rq03c4s80uist78'
+        clientId: '4rrtmgavid4rq03c4s80uist78',
+        googleClientId: '4bkavr8t1jvp2krihuvals3726',
+        hostedUIUrl: 'https://image-detection-login.auth.us-east-1.amazoncognito.com/login?client_id=4rrtmgavid4rq03c4s80uist78&response_type=token&scope=phone+email+openid+aws.cognito.signin.user.admin+profile&redirect_uri=http://localhost:5500/html/main.html'
     },
     api:{
         uploadUrl: 'https://4t07pj5pkk.execute-api.us-east-1.amazonaws.com/dev/api/upload',                 //HTTP POST     JSON Format: {name:"xxxx.jpg", file:""}
